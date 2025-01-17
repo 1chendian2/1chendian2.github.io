f9e1fa956d46e522bc16a772e02c5d8b384e4b74
1chendian2 <chenhongyang86@gmail.com>
2025-01-17 10:35:46 +0800

    Site updated: 2025-01-17 10:35:43

--- js/map.js
+++ js/map.js
@@ -1,1 +1,3 @@
-<script type="text/javascript" id="clustrmaps" src="//clustrmaps.com/map_v2.js?d=rdMbDXtWoUumW7H9naa9M6HnJ1VlByvXQOUEWNqMt84"></script>
+<head>
+<a href="http://www.clustrmaps.com/map/Chenhongyang86.fun" title="Visit tracker for Chenhongyang86.fun"><img src="//www.clustrmaps.com/map_v2.png?d=rdMbDXtWoUumW7H9naa9M6HnJ1VlByvXQOUEWNqMt84" /></a>
+</head>
