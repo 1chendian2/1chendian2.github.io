5ab30516cba4b073e3008fb0f3f4b032f25c0fa8
1chendian2 <chenhongyang86@gmail.com>
2025-06-18 15:32:04 +0800

    Site updated: 2025-06-18 15:31:54

--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
 <head>
 <a href="http://www.clustrmaps.com/map/Chenhongyang86.fun" title="Visit tracker for Chenhongyang86.fun"><img src="//www.clustrmaps.com/map_v2.png?d=rdMbDXtWoUumW7H9naa9M6HnJ1VlByvXQOUEWNqMt84" /></a>
+<script defer src="https://cloud.umami.is/script.js" data-website-id="658e5cfd-feb9-4af8-a8e1-27eb0e7456a8"></script>
 </head>
